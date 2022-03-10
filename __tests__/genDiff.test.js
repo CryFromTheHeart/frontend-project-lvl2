f0d348e2e2854c637343da670cac5da8e773e974
Nikita <nik.mashcov13@mail.ru>
2022-03-10 19:15:49 +0400

    make new tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -9,5 +9,5 @@ const getFile = (filename) => resolve(__dirname, '..', '__fixtures__/', filename
 
 test('main flow', () => {
   expect(genDiff(getFile('file1.json'), getFile('file2.json'))).toBe('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
-  expect(genDiff(getFile('file1.yml'), getFile('file2.yml'))).toBe('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
+  expect(genDiff(getFile('file1.yaml'), getFile('file2.yaml'))).toBe('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
 });
