dad401518e0db4400ebd4402dbf3adb3132246e7
Nikita <nik.mashcov13@mail.ru>
2022-03-19 20:12:03 +0400

    update __tests__/genDiff.test.js

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,16 +1,19 @@
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
-import genDiff from '../src/genDiff.js';
+import fs from 'fs';
+import genDiff from '../src/formatters/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFile = (filename) => resolve(__dirname, '..', '__fixtures__/', filename);
+const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('genDiff beetwen yaml files', () => {
-  expect(genDiff(getFile('file1.yaml'), getFile('file2.yaml'))).toBe('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
+test('genDiff beetwen nested Json files', async () => {
+  const result = await readFile('resultStylish.txt').trim();
+  expect(genDiff(getFixturePath('nestedFile1.json'), getFixturePath('nestedFile2.yaml'), 'stylish')).toBe(result);
 });
-
-test('genDiff beetwen Json files', () => {
-  expect(genDiff(getFile('file1.json'), getFile('file2.json'))).toBe('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
+test('getDiff beetwen nested Json files with plain format', async () => {
+  const result = await readFile('resultPlain.txt').trim();
+  expect(genDiff(getFixturePath('nestedFile1.json'), getFixturePath('nestedFile2.json'), 'plain')).toBe(result);
 });
