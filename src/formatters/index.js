fbc916d488eb9411dbbffa322933374c71d510f1
Nikita <nik.mashcov13@mail.ru>
2022-03-21 18:45:19 +0400

    update src/formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,14 +1,18 @@
 import genDiff from '../genDiff.js';
 import getStylish from './stylish.js';
 import getPlain from './plain.js';
+import getJson from './json.js';
 
-const getDifference = (pathFile1, pathFile2, format) => {
+const getDifference = (pathFile1, pathFile2, format = 'stylish') => {
   const difference = genDiff(pathFile1, pathFile2);
   if (format === 'stylish') {
     return getStylish(difference);
   }
   if (format === 'plain') {
     return getPlain(difference);
+  }
+  if (format === 'json') {
+    return getJson(difference);
   }
   return 'qwe123';
 };
