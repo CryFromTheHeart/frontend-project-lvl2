00caf3bf78d0df7d6def865a6d02759723dcd5b6
Nikita <nik.mashcov13@mail.ru>
2022-03-21 17:35:26 +0400

    update tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -9,11 +9,15 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('genDiff beetwen nested Json files', async () => {
+test('genDiff beetwen files (format \'stylish\')', async () => {
   const result = await readFile('resultStylish.txt').trim();
   expect(genDiff(getFixturePath('nestedFile1.json'), getFixturePath('nestedFile2.yaml'), 'stylish')).toBe(result);
 });
-test('getDiff beetwen nested Json files with plain format', async () => {
+test('getDiff beetwen nested files (format \'plain\')', async () => {
   const result = await readFile('resultPlain.txt').trim();
-  expect(genDiff(getFixturePath('nestedFile1.json'), getFixturePath('nestedFile2.json'), 'plain')).toBe(result);
+  expect(genDiff(getFixturePath('nestedFile1.json'), getFixturePath('nestedFile2.yaml'), 'plain')).toBe(result);
+});
+test('getDiff beetwen nested files (format \'json\')', async () => {
+  const result = await readFile('resultJson.txt').trim();
+  expect(genDiff(getFixturePath('nestedFile1.json'), getFixturePath('nestedFile2.yaml'), 'json')).toBe(result);
 });
