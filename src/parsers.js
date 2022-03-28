afc59c695f12bdf88c2fe3c25c4ce76b4667471e
Nikita <nik.mashcov13@mail.ru>
2022-03-28 21:53:17 +0400

    update src/parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,21 +1,20 @@
 import yaml from 'js-yaml';
 import fs from 'fs';
-import path from 'path';
 
-const parseJson = (file) => JSON.parse(fs.readFileSync(file));
-const parseYaml = (filePath) => yaml.load(fs.readFileSync(filePath, 'utf8'));
+const parseJson = (file) => JSON.parse(file);
+const parseYaml = (file) => yaml.load(file, 'utf8');
 
-const getParse = (filePath) => {
-  const extName = path.extname(filePath);
-
-  if (extName === '.yml' || extName === '.yaml') {
-    return parseYaml(filePath);
+const parse = (file, format) => {
+  switch (format) {
+    case 'yml':
+      return parseYaml(file);
+    case 'yaml':
+      return parseYaml(file);
+    case'json':
+      return parseJson(file);
+    default:
+      throw new Error('format is not support');
   }
-  if (extName === '.json') {
-    return parseJson(filePath);
-  }
-
-  throw new Error('operation is not supported');
 };
 
-export default getParse;
+export default parse;
