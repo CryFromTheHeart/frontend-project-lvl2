0534e357bb27a1b641ba757d609f11a5517ab049
Nikita <nik.mashcov13@mail.ru>
2022-04-08 21:34:46 +0400

    update src/parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,13 +1,13 @@
 import yaml from 'js-yaml';
 
-const parse = (file, format) => {
+const parse = (data, format) => {
   switch (format) {
     case 'yml':
-      return yaml.load(file, 'utf8');
+      return yaml.load(data, 'utf8');
     case 'yaml':
-      return yaml.load(file, 'utf8');
+      return yaml.load(data, 'utf8');
     case 'json':
-      return JSON.parse(file);
+      return JSON.parse(data);
     default:
       throw new Error('format is not support');
   }
